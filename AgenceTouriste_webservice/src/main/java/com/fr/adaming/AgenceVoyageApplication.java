8d1c8fd506944d35e1985d9a188712671be47b88
Ben Salha Mehdi <bensalhamehdi91@gmail.com>
2019-02-24 21:32:51 +0100

    Manage conflict

--- AgenceTouriste_webservice/src/main/java/com/fr/adaming/AgenceVoyageApplication.java
+++ AgenceTouriste_webservice/src/main/java/com/fr/adaming/AgenceVoyageApplication.java
@@ -3,21 +3,12 @@ package com.fr.adaming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-<<<<<<< HEAD
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-=======
->>>>>>> f3039cd1097e60dc3c790ef4b9bcc4587ed6af9b
 
 /**
  * @author Claire
  *
  */
 @SpringBootApplication
-<<<<<<< HEAD
-@EnableSwagger2
-=======
->>>>>>> f3039cd1097e60dc3c790ef4b9bcc4587ed6af9b
 public class AgenceVoyageApplication extends SpringBootServletInitializer {
 
 	/**
